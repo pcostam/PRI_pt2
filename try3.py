40cf09a2c047088835b4b4574efca5bf528cfa19
Paulo Alves <36593311+PauloSanchez@users.noreply.github.com>
2019-11-17 20:21:43 +0000

    coisas fixes

--- try3.py
+++ try3.py
@@ -1,35 +1,47 @@
-
-"""
-Exercise 3
-"""
+"""Exercise 3"""
 from scipy import sparse
 from sklearn.feature_extraction.text import TfidfVectorizer 
 import math
+import operator
 try1 = __import__('try1')
 try2 = __import__('try2')
 
 def main():
     train_set, test_set  = try2.get_dataset("test", t="word", stem_or_not_stem = "not stem")
+    train_set = list(train_set)
     #true_labels = try2.json_references(stem_or_not_stem = "not stem")
-    num = 0
-    for doc in train_set:
-        #doc = try1.open_file()
-        tfidf = tf_idf([doc])
-        tf = do_tf([doc])
-        idf = do_idf([doc], 4)
+    
+    #doc = try1.open_file()
+    vectorizer = tf_idf_train(train_set)
+    vectorizer_tf = do_tf_train(train_set)
+        
+    for test_doc in list(test_set.values())[:15]:
+        tfidf = vectorizer.transform(test_doc)
+        tf    = vectorizer_tf.transform(test_doc)
+        idf   = vectorizer.idf_
+        
+        tfidf = dict(zip(vectorizer.get_feature_names(), tfidf.data.tolist()))
+        tf    = dict(zip(vectorizer_tf.get_feature_names(), tf.data.tolist()))
+        idf   = dict(zip(vectorizer.get_feature_names(), idf.data.tolist()))
+        
         
         rankers = [tfidf, tf, idf]
-        RRF = RRFScore(rankers)
+        RRF     = RRFScore(rankers)
         CombSum = CombSumScore(rankers)
         CombMNZ = CombMNZScore(rankers)
         
-        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>DOC NUM:", num)
-        print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
-        print("RRF:", RRF)
-        print("CombSum:", CombSum)
-        print("CombMNZ:", CombMNZ)
-        num +=1
-
+        RRF     = sorted(RRF.items()    , key = operator.itemgetter(1), reverse = True)
+        CombSum = sorted(CombSum.items(), key = operator.itemgetter(1), reverse = True)
+        CombMNZ = sorted(CombMNZ.items(), key = operator.itemgetter(1), reverse = True)
+        
+        print(">>>RRF: "    ,RRF[:5])
+        print(">>>CombSum: ",CombSum[:5])
+        print(">>>CombMNZ: ",CombMNZ[:5])            
+    
+        #print("RRF:", RRF)
+        #print("CombSum:", CombSum)
+        #print("CombMNZ:", CombMNZ)
+    
 def CombMNZScore(rankers):
     dictCombMNZ = dict()
     #number of ranks where term occurs
@@ -69,41 +81,7 @@ def RRFScore(rankers):
                     dictRRF[key] += 1/(50+rank)                
     return dictRRF
 
-def do_count(doc):
-     vectorizer_tf = TfidfVectorizer(binary=True, 
-                                            use_idf = False, 
-                                           analyzer = 'word', 
-                                           ngram_range=(1,3), 
-                                           stop_words = 'english',
-                                           token_pattern=r"(?u)\b[a-zA-Z][a-zA-Z-]*[a-zA-Z]\b", 
-                                           lowercase = True,
-                                           norm = None)
-     #term-document matrix
-     matrix = vectorizer_tf.fit_transform(doc)
-     matrix = sparse.csr_matrix(matrix)
-     matrix = matrix.tocoo()
-     #print("matrix", matrix)
-  
-     feature_names = vectorizer_tf.get_feature_names()
-     countDict = dict()
-     for col, data in zip(matrix.col, matrix.data):
-        name = feature_names[col]
-        if name not in countDict.keys():
-            countDict[name] = data
-        else:
-            countDict[name] += data
-    
-
-     return countDict
- 
-def do_idf(docs, total_no_docs):
-    count = do_count(docs)
-    dictIdf = dict()
-    for term_name, count in count.items():
-         dictIdf[term_name] = math.log(total_no_docs/count)
-    return dictIdf
-
-def do_tf(doc):
+def do_tf_train(doc):
     vectorizer_tf = TfidfVectorizer(use_idf = False, 
                                            analyzer = 'word', 
                                            ngram_range=(1,3), 
@@ -111,32 +89,10 @@ def do_tf(doc):
                                            token_pattern=r"(?u)\b[a-zA-Z][a-zA-Z-]*[a-zA-Z]\b", 
                                            lowercase = True,
                                            norm = 'l1')
-    matrix = vectorizer_tf.fit_transform(doc)
-    test_vector = sparse.csr_matrix(matrix[0])
-    test_vector = test_vector.tocoo()
-    
-    dictTf = {}
-    feature_names = vectorizer_tf.get_feature_names()
-    for col, data in zip(test_vector.col, test_vector.data):
-        name = feature_names[col]
-        dictTf[name] = data
-    return dictTf
-    
-def tf_idf(doc):
-    #TRAIN
-    #print("doc", doc)
-    test_vector, vectorizer = tf_idf_train(doc)
-    feature_names = vectorizer.get_feature_names()
-    tfidfDict = do_tfidf(test_vector, feature_names)
-    #print("tfIdfDict", tfidfDict)
-    return tfidfDict
-  
-def do_tfidf(test_vector, feature_names):  
-    dictTfidf = {}
-    for col, data in zip(test_vector.col, test_vector.data):
-        name = feature_names[col]
-        dictTfidf[name] = data
-    return dictTfidf
+    vectorizer_tf.fit_transform(doc)
+        
+    return vectorizer_tf
+
 
 #Creates vectorizer and fits it to the docs
 #@input:train set, parameter (optional) removes the n most frequent words 
@@ -152,12 +108,14 @@ def tf_idf_train(docs, maxdf = 1, mindf = 1):
                                            min_df = mindf,
                                            norm = 'l1')
         
-    matrix = vectorizer_tfidf.fit_transform(docs)
-    test_vector = sparse.csr_matrix(matrix[0])
-    test_vector = test_vector.tocoo()
+    vectorizer_tfidf.fit_transform(docs)
     #print("test_vector", test_vector)
+    
+    return vectorizer_tfidf
 
-        
-    return test_vector, vectorizer_tfidf
-  
-
+def do_tfidf_test(test_vector, feature_names):  
+    dictTfidf = {}
+    for col, data in zip(test_vector.col, test_vector.data):
+        name = feature_names[col]
+        dictTfidf[name] = data
+    return dictTfidf
