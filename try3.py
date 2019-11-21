b7c4fa38fed3e4e8ecb8504367dbb964c985453e
Paulo Alves <36593311+PauloSanchez@users.noreply.github.com>
2019-11-21 19:12:16 +0000

    nao sou desses

--- try3.py
+++ try3.py
@@ -21,37 +21,43 @@ def main():
     
     
     #doc = try1.open_file()
-    vectorizer = tf_idf_train(train_set, words_nodes)
-    vectorizer_tf = do_tf_train(train_set, words_nodes)
+    vectorizer = tf_idf_train(train_set, words_nodes, maxdf=0.5, mindf=2)
+    vectorizer_tf = do_tf_train(train_set, words_nodes, maxdf=0.5, mindf=2)
     
     i = 0
     all_ap_RRF = list()
     all_ap_CombSum = list()
     all_ap_CombMNZ = list()
     for key, test_doc in test_set.items():
+        print("key", key)
         y_true = true_labels[key]
-        
-        tfidf = vectorizer.transform(test_doc)
-        tf    = vectorizer_tf.transform(test_doc)
+        print("y_true", y_true)
+        tfidf_vector = vectorizer.transform(test_doc)
+     
+        tf_vector    = vectorizer_tf.transform(test_doc)
         idf   = vectorizer.idf_
+     
+        tfidf_name = map_name_score(tfidf_vector.tocoo(), vectorizer.get_feature_names())
+        tf_name    = map_name_score( tf_vector.tocoo(), vectorizer_tf.get_feature_names())
+        idf_name   = dict(zip( vectorizer.get_feature_names(), idf))
         
-        tfidf = dict(zip(vectorizer.get_feature_names(), tfidf.data.tolist()))
-        tf    = dict(zip(vectorizer_tf.get_feature_names(), tf.data.tolist()))
-        idf   = dict(zip(vectorizer.get_feature_names(), idf.data.tolist()))
         
-        
-        rankers = [tfidf, tf, idf]
+        rankers = [tfidf_name, tf_name, idf_name]
         RRF     = RRFScore(rankers)
         CombSum = CombSumScore(rankers)
         CombMNZ = CombMNZScore(rankers)
         
-        RRF     = sorted(RRF.items()    , key = operator.itemgetter(1), reverse = True)
-        CombSum = sorted(CombSum.items(), key = operator.itemgetter(1), reverse = True)
-        CombMNZ = sorted(CombMNZ.items(), key = operator.itemgetter(1), reverse = True)
+        RRF_sorted     = sorted(RRF.items()    , key = operator.itemgetter(1), reverse = True)
+        CombSum_sorted = sorted(CombSum.items(), key = operator.itemgetter(1), reverse = True)
+        CombMNZ_sorted = sorted(CombMNZ.items(), key = operator.itemgetter(1), reverse = True)
         
-        y_pred_RRF = [i[0] for i in RRF[:5]] 
-        y_pred_CombSum = [i[0] for i in CombSum[:5]]
-        y_pred_CombMNZ = [i[0] for i in CombMNZ[:5]]
+        y_pred_RRF = [i[0] for i in RRF_sorted[:5]] 
+        y_pred_CombSum = [i[0] for i in CombSum_sorted[:5]]
+        y_pred_CombMNZ = [i[0] for i in CombMNZ_sorted[:5]]
+        
+        print(y_pred_RRF)
+        print(y_pred_CombSum)
+        print(y_pred_CombMNZ)
         
         RRF_avg_precision     = average_precision_score(y_true, y_pred_RRF)
         all_ap_RRF.append(RRF_avg_precision)
@@ -74,7 +80,7 @@ def main():
         #print("CombMNZ:", CombMNZ)
         
     
-        if(i == 14):
+        if(i == 80):
           break
         else:
             i += 1
@@ -83,11 +89,20 @@ def main():
     mean_average_score_CombSum = np.array(all_ap_CombSum)/len(all_ap_CombSum)
     mean_average_score_CombMNZ = np.array(all_ap_CombSum)/len(all_ap_CombMNZ)
     
-    print("RRF_avg_precision",  mean_average_score_RRF)
-    print("CombSum_avg_precision", mean_average_score_CombSum )
-    print("CombMNZ_avg_precision",  mean_average_score_CombMNZ)
+    #print("RRF_avg_precision",  mean_average_score_RRF)
+    #print("CombSum_avg_precision", mean_average_score_CombSum )
+    #print("CombMNZ_avg_precision",  mean_average_score_CombMNZ)
     
-    
+def vector_scores(test_vector, feature_names):
+    test_vector = test_vector.toarray()
+   
+    for i in range(0, test_vector.shape[0]):
+        for j in range(0, test_vector.shape[1]):
+            if test_vector[i,j] != 0:
+                    test_vector[i,j] =  test_vector[i,j] * len(feature_names[j].split())
+                    
+    test_vector = sparse.csr_matrix(test_vector)
+    return test_vector
 def CombMNZScore(rankers):
     dictCombMNZ = dict()
     #number of ranks where term occurs
@@ -127,14 +142,15 @@ def RRFScore(rankers):
                     dictRRF[key] += 1/(50+rank)                
     return dictRRF
 
-def do_tf_train(doc, vocab):
+def do_tf_train(doc, vocab, maxdf=1, mindf=1):
     vectorizer_tf = TfidfVectorizer(use_idf = False, 
                                            analyzer = 'word', 
                                            ngram_range=(1,3), 
                                            stop_words = 'english',
-                                           token_pattern=r"(?u)\b[a-zA-Z][a-zA-Z-]*[a-zA-Z]\b", 
                                            lowercase = True,
                                            norm = 'l1',
+                                           max_df=maxdf,
+                                           min_df=mindf,
                                            vocabulary=vocab)
     vectorizer_tf.fit_transform(doc)
         
@@ -148,8 +164,7 @@ def tf_idf_train(docs, vocab,  maxdf = 1, mindf = 1):
     vectorizer_tfidf = TfidfVectorizer(use_idf = True, 
                                            analyzer = 'word', 
                                            ngram_range=(1,3), 
-                                           stop_words = 'english',
-                                           token_pattern=r"(?u)\b[a-zA-Z][a-zA-Z-]*[a-zA-Z]\b", 
+                                           stop_words = 'english', 
                                            lowercase = True,
                                            max_df = maxdf,
                                            min_df = mindf,
@@ -161,7 +176,7 @@ def tf_idf_train(docs, vocab,  maxdf = 1, mindf = 1):
     
     return vectorizer_tfidf
 
-def do_tfidf_test(test_vector, feature_names):  
+def map_name_score(test_vector, feature_names):  
     dictTfidf = {}
     for col, data in zip(test_vector.col, test_vector.data):
         name = feature_names[col]
