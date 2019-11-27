e7986eb9c632ade65ffd636b3c6a1bd73f8ce85e
pcostam <pcosta.margarida@gmail.com>
2019-11-27 17:12:58 +0000

    dictionaries by category

--- try4.py
+++ try4.py
@@ -3,33 +3,57 @@
 Exercise-4
 """
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup 
 import re
 from urllib.request import urlopen
 from xml.etree.ElementTree import parse
 
 def main():
     doc = urlopen("https://archive.nytimes.com/www.nytimes.com/services/xml/rss/index.html").read()
-    links_xml = extract(doc)
-    files = [urlopen(el) for el in links_xml]
-    docs = list()
-    for f in files:
-        xmldoc = parse(f)
-        
-        for item in xmldoc.iterfind('channel/item'):
-            title = item.findtext('title')
-            desc = item.findtext('description')
-            doc = [title + " " + desc]
-            docs.append(doc)
+    links_xml_dict = extract(doc)
+    files = dict()
+    docs = dict()
+    for key, value in links_xml_dict.items():
+        if key not in files.keys():
+            files[key] = [urlopen(el) for el in value]
+        else:
+            files[key] += [urlopen(el) for el in value]
+   
+
+     
+    for key, value in files.items():
+        for f in value:
+            xmldoc = parse(f)
+            
+            for item in xmldoc.iterfind('channel/item'):
+                title = item.findtext('title')
+                desc = item.findtext('description')
+                doc = [title + " " + desc]
+                docs[key] = doc
+    print(docs)
        
 
        
 def extract(content):
-    links = []
+    links = list()
+    cat_dict = dict()
     soup = BeautifulSoup(content, 'lxml')
     for tag in soup.find_all():
+        if tag.name == 'div' and 'class' in tag.attrs.keys() and tag.attrs['class'] == ['columnGroup', 'singleRule']:
+            category = ''
+                
+        if tag.name == 'span' and 'class' in tag.attrs.keys() and tag.attrs['class']==['rssRow' ]:
+                category = tag.get_text()
+                print("category", category)
+                
         if tag.name == 'a' and 'href' in tag.attrs:
-            link = tag.attrs['href']
-            if(re.match(".+\.xml{1}", link)): 
-                    links.append(link)
-    return links
+                link = tag.attrs['href']
+                
+                if(re.match(".+\.xml{1}", link)): 
+                    if category not in cat_dict.keys():
+                        cat_dict[category] = [link]
+                    else:
+                        cat_dict[category] += [link]
+                        links.append(link)
+                        
+    return cat_dict
