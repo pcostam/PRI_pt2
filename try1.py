d9f1bf4675ebc8165f3aff9f93e0e92a4b23910e
martasaparicio <36605729+martasaparicio@users.noreply.github.com>
2019-12-03 19:33:35 +0000

    cleaned

--- try1.py
+++ try1.py
@@ -1,31 +1,24 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Nov  5 18:59:16 2019
-
-@author: LADYMARTINHA
+EXERCISE_1
 """
-#https://xang1234.github.io/textrank/ == corpus
 import sys
 import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords 
 import string
-#from nltk.stem import WordNetLemmatizer 
 import networkx as nx
-from collections import Counter 
 import regex as re
 
-#try2 = __import__('try2')
-
+#https://xang1234.github.io/textrank/ == corpus
 fIn = "corpus.txt"
 stopwords_punctuation  = set(stopwords.words('english')).union(string.punctuation)
 
 def main():
     file_content = open_file()
-    nodes = extractKeyphrasesTextRank(str(file_content), exercise2=True)
-    graph = buildGraph(nodes, exercise2 = False)    
+    nodes = extractKeyphrasesTextRank(str(file_content))
+    graph = buildGraph(nodes, exercise2 = False)  
     pagerank_scores = nx.pagerank(graph, max_iter = 50)
-    #print(pagerank_scores)
     return get_top_x(pagerank_scores, 5)
 
 def open_file():
@@ -41,23 +34,20 @@ def open_file():
     except: #handle other exceptions such as attribute errors
        print("Unexpected error:", sys.exc_info()[0])
        
-def extractKeyphrasesTextRank(file_content, tags = ['NN', 'NNS', 'NNP', 'NNPS', 'JJ', 'JJR', 'JJS'], exercise2 = False):
+def extractKeyphrasesTextRank(file_content, tags = ['NN', 'NNS', 'NNP', 'NNPS', 'JJ', 'JJR', 'JJS']):
+    
     # tokenize into sentences
     sent_tokens = sent_tokenize(file_content)
-
-    #print(sent_tokens)
     
-    # remove stopwords, punctuation also makes lowering and lemmanize
+    # remove stopwords, punctuation and digits also makes lowering
     sent_tokens_clean =[]
-    #lmtzr = WordNetLemmatizer()
     for sent in sent_tokens:
         lst=[]
         for word in word_tokenize(sent):
             if (word.lower() not in stopwords_punctuation) and not(re.match('\d+', word)):
                 lst.append(word.lower())
-                #lst.append(lmtzr.lemmatize(word.lower()))
         sent_tokens_clean.append(' '.join(lst))
-    #print(sent_tokens_clean)
+        
     # assign POS tags to the words in the text
     sent_tokens_filtered = []
     for sent in sent_tokens_clean:    
@@ -65,7 +55,7 @@ def extractKeyphrasesTextRank(file_content, tags = ['NN', 'NNS', 'NNP', 'NNPS', 
         for item in tagged_sents:
             if item[1] in tags and sent not in sent_tokens_filtered:
                 sent_tokens_filtered.append(sent)
-    #print(">>>", sent_tokens_filtered)
+                
     #List with uni- bi- and trigrams per sentence
     words_nodes=list()
     for sent in sent_tokens_filtered:
@@ -83,20 +73,11 @@ def extractKeyphrasesTextRank(file_content, tags = ['NN', 'NNS', 'NNP', 'NNPS', 
         for gram in trigram:
             sent_grams.append(' '.join(gram))
             
-        #print(sent_grams)
-        #words_nodes +=[sent_grams_set]
-        words_nodes.append(sent_grams)
-    #print("word_nodes>>", words_nodes) 
-    
-        #List unique elements, preserving order. Remember all elements ever seen.
-        #sent_grams_set = list(unique_everseen(sent_grams))
-        #print(sent_grams_set)               
-
+        words_nodes.append(sent_grams)           
     return words_nodes
       
 def buildGraph(nodes, edge_weights = [], exercise2 = False): 
     gr = nx.Graph()  # initialize an undirected graph
-    #print("nodes>>>>", nodes)
     for sent in nodes:
         gr.add_nodes_from(sent)
         for gram1 in range(0, len(sent)):
@@ -104,11 +85,11 @@ def buildGraph(nodes, edge_weights = [], exercise2 = False):
                 if sent[gram1] != sent[gram2] and sent[gram1] not in sent[gram2] and sent[gram2] not in sent[gram1]:
                     if  exercise2 == False:
                         gr.add_edge(sent[gram1], sent[gram2], weight = 1)
-                        print("add edge>>", sent[gram1], sent[gram2])
     if exercise2 == True:
         gr.add_weighted_edges_from(edge_weights)
     return gr
 
+from collections import Counter 
 def get_top_x(pagerank_scores, x):
     k = Counter(pagerank_scores)
     top_x = k.most_common(x)
